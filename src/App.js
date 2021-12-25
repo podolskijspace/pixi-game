b4fafa1f14b6dc969917df3f2696c025e66f42a9
podolskijspace <goncharovnikita713@gmail.com>
2021-12-25 12:32:54 +0500

    Sheep moving left and right

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import { Stage, Sprite } from '@inlet/react-pixi'
-import {use}
+import { Stage, Container, Sprite, useTick } from '@inlet/react-pixi'
+import {useState, useEffect, useCallback} from 'react'
 
 import sheep1 from './assets/images/sheeps/sheep-1.png'
 import './App.css';
 
 function App() {
+  const [sheepPosition, setSheepPositon] = useState(200);
+  const [moveLeft, setMoveLeft] = useState(false)
+  const [moveRight, setMoveRight] = useState(false)
+  const [sheepSpeed, setSheepSpeed] = useState(2)
 
+  //События клавиатуры
+
+  const onKeyDown = useCallback((e) => {
+    e.preventDefault();
+    const code = e.keyCode
+    if (code === 37 || code === 65) {
+      setMoveLeft(true)
+    } else if (code === 39 || code === 68) {
+      setMoveRight(true)
+    }
+  }, [])
+  const onKeyPress = (e) => {
+    e.preventDefault();
+  }
+  const onKeyUp = (e) => {
+    e.preventDefault();
+    const code = e.keyCode
+    if (code === 37 || code === 65) {
+      setMoveLeft(false)
+    } else if (code === 39 || code === 68) {
+      setMoveRight(false)
+    }
+  }
+
+  //Движок игры
+  const moveLeftAct = () => {
+      setSheepPositon(sheepPosition - 1 * sheepSpeed)
+  }
+
+  const moveRightAct = () => {
+    setSheepPositon(sheepPosition + 1 * sheepSpeed)
+  }
+
+  useTick(moveLeftAct, moveLeft);
+  useTick(moveRightAct, moveRight);
+
+  //Добавление и удаление событий клавиатуры
+  const addListeners = () => {
+    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keyup', onKeyUp)
+    document.addEventListener('keypress', onKeyPress)
+  }
+  const removeListeners = () => {
+    document.removeEventListener('keydown', onKeyDown)
+    document.removeEventListener('keyup', onKeyUp)
+    document.removeEventListener('keypress', onKeyPress)
+  }
+  const makeListenres = () => {
+    addListeners()
+    return removeListeners
+  }
+
+  useEffect (makeListenres, [])
 
   return (
-    <div className="App">
-      <Stage width={400} height={686} options={{ backgroundColor: 0xeef1f5 }}>
-        <Sprite
-          image={sheep1}
-          anchor={{ x: 0.5, y: 1 }}
-          x={200}
-          y={686}
-          width={60}
-          height={60}
-        />
-      </Stage>
-    </div>
+    <Container>
+      <Sprite
+        image={sheep1}
+        anchor={{ x: 0.5, y: 1 }}
+        x={sheepPosition}
+        y={686}
+        width={80}
+        height={60}
+      />
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Stage } from '@inlet/react-pixi'
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Stage width={400} height={686} options={{ backgroundColor: 0xeef1f5 }}>
+
+      <App />
+    </Stage>
   </React.StrictMode>,
   document.getElementById('root')
 );
